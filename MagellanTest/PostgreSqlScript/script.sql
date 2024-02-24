afeeba7796358180e21b2465c39cfc54c874035c
mMartinez00 <martinez.mauricio.0093@gmail.com>
2024-02-24 17:13:08 -0500

    Get_Total_Cost function complete

--- MagellanTest/PostgreSqlScript/script.sql
+++ MagellanTest/PostgreSqlScript/script.sql
@@ -22,3 +22,31 @@ VALUES ('Item1', NULL, 500, '2024-02-20'),
 ('Sub4', 2, 400, '2024-01-02'),
 ('Item2', NULL, 600, '2024-03-15'),
 ('Sub1', 6, 200, '2024-02-25');
+
+
+CREATE FUNCTION Get_Total_Cost(item_name_input VARCHAR(50))
+RETURNS INT AS $$ 
+DECLARE 
+    total_cost INT; 
+BEGIN 
+    IF (SELECT parent_item FROM item WHERE item_name = item_name_input) IS NULL THEN
+         WITH RECURSIVE my_func(id, item_name, parent_item, cost) AS (
+            SELECT id, item_name, parent_item, cost 
+            FROM item 
+            WHERE item_name = item_name_input
+            
+            UNION ALL
+            
+            SELECT i.id, i.item_name, i.parent_item, i.cost 
+            FROM item i
+            JOIN my_func mf ON i.parent_item = mf.id
+        )
+        SELECT SUM(cost) INTO total_cost FROM my_func; 
+        RETURN total_cost;
+    ELSE 
+        RETURN total_cost;
+    END IF;
+END; 
+$$ LANGUAGE plpgsql;
+
+SELECT Get_Total_Cost('Sub2');
